dc9379acbb5275afc63fbe974713ee5fd04058c8
Yuri <yuricarneiroparente@hotmail.com>
2023-05-03 17:28:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.tree import DecisionTreeClassifier
 
 
 #funções para encontrar e remover os outliers
@@ -54,8 +56,10 @@ print(dados.isnull().sum())
 
 print("O dataset possui {} linhas e {} colunas".format(dados.shape[0], dados.shape[1]))
 
-# Removendo as colunas com mais de 90% de valores faltantes
+# Removendo as colunas com mais de 90% de valores faltantes e o id do paciente
 dados = dados.loc[:, dados.isnull().mean() < 0.9]
+
+dados = dados.drop("Patient ID", axis=1)
 
 print("O dataset possui {} linhas e {} colunas".format(dados.shape[0], dados.shape[1]))
 
@@ -85,8 +89,9 @@ for coluna in df_final.columns:
 
 
 
-corr_final = df_final.corr(numeric_only=True)
+#corr_final = df_final.corr(numeric_only=True)
 
+#Preenchendo lacunas vazias
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 df_final = pd.DataFrame(imputer.fit_transform(df_final), columns=df_final.columns)
 
@@ -104,38 +109,64 @@ df_final = pd.DataFrame(imputer.fit_transform(df_final), columns=df_final.column
 print(df_final)
 
 X = df_final.drop("SARS-Cov-2 exam result", axis=1)
-X = pd.get_dummies(X)
+#X = pd.get_dummies(X)
 y = df_final["SARS-Cov-2 exam result"]
 
-melhor_resultado = 0
-melhor_semente = 0
-mc = []
-
-#For para testar sementes de 0 a 1000 
-for semente in range(1000):
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=semente)
-
-    
-    # Criação e treinamento do modelo KNN
-    modelo = KNeighborsClassifier(n_neighbors=5)
-    modelo.fit(X_train, y_train)
-
-    # Avaliação do modelo
-    y_pred = modelo.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    
+# melhor_resultado = 0
+# melhor_semente = 0
+# mc = []
 
 
-    if accuracy > melhor_resultado:
-        melhor_resultado = accuracy
-        melhor_semente = semente
-        print('Acurácia do modelo: {:.2f}%'.format(accuracy * 100))
-        # mc = confusion_matrix(y_test, y_pred)
-        # print(mc)
-        
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 
-print("Melhor semente:",melhor_semente,"Melhor resultado:", melhor_resultado)
+#TESTANDO KNN
+# Criação e treinamento do modelo KNN
+modelo = KNeighborsClassifier(n_neighbors=5)
+modelo.fit(X_train, y_train)
+
+# Avaliação do modelo
+y_pred = modelo.predict(X_test)
+precisao = accuracy_score(y_test, y_pred)
+
+
+# print("Melhor semente:",melhor_semente,"Melhor resultado:", melhor_resultado)
+print('Acurácia do knn: {:.2f}%'.format(precisao * 100))
+mc = confusion_matrix(y_test, y_pred)
+print(mc)
+
+
+#TESTANDO ÁRVORE DE DECISÃO
+tree = DecisionTreeClassifier(max_depth=3)
+
+# Treina o modelo usando o conjunto de treinamento
+tree.fit(X_train, y_train)
+
+# Faz a predição usando o conjunto de teste
+y_pred = tree.predict(X_test)
+
+# Calcula a acurácia do modelo
+precisao = tree.score(X_test, y_test)
+
+# Imprime a acurácia do modelo
+print("Acurácia da árvore de decisão: {:.2f}%".format(precisao*100))
+mc = confusion_matrix(y_test, y_pred)
+print(mc)
+
+
+#TESTANDO NAIVE BAYES
+# Instancia o modelo de Naive Bayes
+modelo = GaussianNB()
+
+# Treina o modelo
+modelo.fit(X_train, y_train)
+
+# Faz as previsões no dataset de teste
+y_pred = modelo.predict(X_test)
+
+# Avalia a acurácia do modelo
+precisao = accuracy_score(y_test, y_pred)
+print("Acurácia do naive bayes:{:.2f}%".format(precisao*100))
 mc = confusion_matrix(y_test, y_pred)
 print(mc)
