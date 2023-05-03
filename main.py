6262f10aac742a376086f07f320046027f09adf6
Yuri <yuricarneiroparente@hotmail.com>
2023-05-02 21:41:51 -0300

    Alterei a forma de limpar os dados e adicionei o algoritmo de treinamento knn

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 
 #funções para encontrar e remover os outliers
@@ -10,7 +15,7 @@ def outliers(dataFrame, nome):
 
     lb = q1 - 1.5 * iqr
     ub = q3 + 1.5 * iqr
-    #lista = ["pica"]
+    
     lista = dataFrame.index[ (dataFrame[nome] < lb) | (dataFrame[nome] > ub ) ]
 
     return lista
@@ -21,7 +26,7 @@ def remover_outliers(dataFrame, lista):
 
     lista = sorted(set(lista))
     dataFrame = dataFrame.drop(lista)
-    print("calma la espera")
+    
     return dataFrame
 
 
@@ -42,51 +47,97 @@ print(dados.isnull().sum())
 
 #como o dataset tem muitos valores vazios
 #serão removidos aqueles individuos que não possuem todas as informações sobre o exame de sangue completas
-dados_limpo = dados.dropna(subset=['Hematocrit','Hemoglobin', 'Platelets','Mean platelet volume ', 'Red blood Cells','Lymphocytes','Mean corpuscular hemoglobin concentration (MCHC)','Leukocytes', 'Basophils','Mean corpuscular hemoglobin (MCH)','Eosinophils','Mean corpuscular volume (MCV)', 'Monocytes','Red blood cell distribution width (RDW)','Serum Glucose'])
+# dados_limpo = dados.dropna(subset=['Hematocrit','Hemoglobin', 'Platelets','Mean platelet volume ', 'Red blood Cells','Lymphocytes','Mean corpuscular hemoglobin concentration (MCHC)','Leukocytes', 'Basophils','Mean corpuscular hemoglobin (MCH)','Eosinophils','Mean corpuscular volume (MCV)', 'Monocytes','Red blood cell distribution width (RDW)','Serum Glucose'])
 
 
 
 
-print("O dataset possui {} linhas e {} colunas".format(dados_limpo.shape[0], dados_limpo.shape[1]))
+print("O dataset possui {} linhas e {} colunas".format(dados.shape[0], dados.shape[1]))
+
+# Removendo as colunas com mais de 90% de valores faltantes
+dados = dados.loc[:, dados.isnull().mean() < 0.9]
+
+print("O dataset possui {} linhas e {} colunas".format(dados.shape[0], dados.shape[1]))
 
 #filtrando mais um pouco removendo aqueles que possuem muitos outros dados faltando
-dados_limpo.dropna(axis=1,thresh=55, inplace=True)
+dados.dropna(axis=1,thresh=55, inplace=True)
 
-print(dados_limpo.isnull().sum())
+print(dados.isnull().sum())
 
 #usando IRQ para remover outliers
- 
-print(dados_limpo.columns)
 lista_index = []
-for nome in dados_limpo.columns:
+for nome in dados.columns:
     print(nome)
-    if dados_limpo[nome].dtypes == float:
-        lista_index.extend(outliers(dados_limpo, nome))
+    if dados[nome].dtypes == float:
+        lista_index.extend(outliers(dados, nome))
         
     
 
-print(lista_index)
-
-df_final = remover_outliers(dados_limpo, lista_index)
-
-print(df_final)
 
 
-#alterando alguns valores para que o dataset fique inteiramente numérico
-df_final = df_final.replace(['negative'], 0)
-df_final = df_final.replace(['positive'], 1)
-df_final = df_final.replace(['not_detected'],0)
-df_final = df_final.replace(['detected'],1)
+df_final = remover_outliers(dados, lista_index)
+
+
+
+#alterando os valores para que o dataset fique inteiramente numérico
+
+for coluna in df_final.columns:
+    if df_final[coluna].dtype == "object":
+        df_final[coluna] = pd.Categorical(df_final[coluna]).codes
 
 
 
 corr_final = df_final.corr(numeric_only=True)
+
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+df_final = pd.DataFrame(imputer.fit_transform(df_final), columns=df_final.columns)
 
 #armazenando a correlação em um arquivo exel para algum tipo de análise visual 
 #corr_final.to_excel(excel_writer='C:/Users/Lenovo/Desktop/testeExcel/arquivo.xlsx')
 #remover a linha a cima caso queira testar com um diretório na propria maquina
 
 #testando matshow para visualizar, porém sem muito sucesso. Seria necessário configurar melhor para uma melhor análise ou analisar atrávez do excel 
-plt.matshow(corr_final)
-plt.show()
 
+# plt.matshow(corr_final)
+# plt.show()
+
+# Separa os dados em conjunto de treinamento e teste
+
+print(df_final)
+
+X = df_final.drop("SARS-Cov-2 exam result", axis=1)
+X = pd.get_dummies(X)
+y = df_final["SARS-Cov-2 exam result"]
+
+melhor_resultado = 0
+melhor_semente = 0
+mc = []
+
+#For para testar sementes de 0 a 1000 
+for semente in range(1000):
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=semente)
+
+    
+    # Criação e treinamento do modelo KNN
+    modelo = KNeighborsClassifier(n_neighbors=5)
+    modelo.fit(X_train, y_train)
+
+    # Avaliação do modelo
+    y_pred = modelo.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    
+
+
+    if accuracy > melhor_resultado:
+        melhor_resultado = accuracy
+        melhor_semente = semente
+        print('Acurácia do modelo: {:.2f}%'.format(accuracy * 100))
+        # mc = confusion_matrix(y_test, y_pred)
+        # print(mc)
+        
+
+
+print("Melhor semente:",melhor_semente,"Melhor resultado:", melhor_resultado)
+mc = confusion_matrix(y_test, y_pred)
+print(mc)
